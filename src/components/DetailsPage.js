f2817212728b46766a4b679b1e533fa4f293e0f4
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-08-07 04:43:18 -0600

    update style and components

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,23 +1,36 @@
+/* eslint-disable react/prop-types */
 // src/components/DetailsPage.js
-
 import React, { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { FaArrowLeft } from 'react-icons/fa';
+import { FiSettings, FiChevronLeft } from 'react-icons/fi';
+import { FaBell } from 'react-icons/fa';
 import { fetchCoordinates } from '../actions';
+import airPurityIcon from '../assets/air-purity.PNG';
+import statesImages from '../data/mexico.json';
+
+const determineAirQuality = (aqi) => {
+  if (aqi <= 50) {
+    return 'clean';
+  } if (aqi <= 100) {
+    return 'intermediate';
+  }
+  return 'bad';
+};
 
 const DetailsPage = ({
   fetchCoordinates, data, loading, error,
 }) => {
   const { stateName } = useParams();
 
+  // Encuentra la información de la imagen basada en el nombre del estado
+  const selectedState = statesImages.find((state) => state.estado === stateName);
+
   useEffect(() => {
     fetchCoordinates(stateName);
   }, [stateName, fetchCoordinates]);
 
-  // Ahora se puede renderizar la información de la API como se prefiera.
-  // Aquí se muestra un ejemplo sencillo, pero se puede hacer mucho más con estos datos
   if (loading) return <p>Loading...</p>;
   if (error) {
     return (
@@ -30,83 +43,121 @@ const DetailsPage = ({
   }
   return (
     <div>
+      <div className="logo-container">
+        <img src={airPurityIcon} alt="Air Purity Logo" className="app-logo" />
+      </div>
+
+      <div className="icon-container">
+        <FaBell className="icon" />
+        <FiSettings className="icon" />
+      </div>
       <Link to="/">
-        <FaArrowLeft style={{ fontSize: '1.5em', margin: '10px' }} />
+        <FiChevronLeft style={{ fontSize: '1.5em', margin: '10px', color: 'white' }} />
       </Link>
-      <h1> Air Quality</h1>
-      {data.map((item) => (
-        <div key={item.dt}>
-          <h2>{new Date(item.dt * 1000).toLocaleDateString()}</h2>
-          <p>
-            AQI:
-            {' '}
-            {item.main.aqi}
-          </p>
-          <p>
-            CO (Monóxido de Carbono):
-            {' '}
-            {item.components.co}
-            {' '}
-            μg/m3
-          </p>
-          <p>
-            NO (Monóxido de Nitrógeno):
-            {' '}
-            {item.components.no}
-            {' '}
-            μg/m3
-          </p>
-          <p>
-            NO2 (Dióxido de Nitrógeno):
-            {' '}
-            {item.components.no2}
-            {' '}
-            μg/m3
-          </p>
-          <p>
-            O3 (Ozono):
-            {' '}
-            {item.components.o3}
-            {' '}
-            μg/m3
-          </p>
-          <p>
-            SO2 (Dióxido de Azufre):
-            {' '}
-            {item.components.so2}
-            {' '}
-            μg/m3
-          </p>
-          <p>
-            PM2.5:
-            {' '}
-            {item.components.pm2_5}
-            {' '}
-            μg/m3
-          </p>
-          <p>
-            PM10:
-            {' '}
-            {item.components.pm10}
-            {' '}
-            μg/m3
-          </p>
-          <p>
-            NH3 (Amoniaco):
-            {' '}
-            {item.components.nh3}
-            {' '}
-            μg/m3
-          </p>
+      {selectedState && (
+      <div className="state-details-image-container">
+        <img src={selectedState.url} alt={selectedState.estado} className="state-details-image" />
+        <div className="state-details-overlay">
+          <h1 className="state-details-title">{stateName}</h1>
+          {data.length > 0 && (
+            <h2 className="state-details-subtitle">
+              {new Date(data[0].dt * 1000).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
+            </h2>
+          )}
         </div>
+      </div>
+      )}
+      <div className="header-container">
+        <h2 className="quality-header">
+          AIR QUALITY
+        </h2>
+        {/* Si hay datos de calidad del aire, mostramos el indicador */}
+        {data.length > 0 && (
+          <span className={`quality-indicator ${determineAirQuality(data[0].main.aqi)}`} title={determineAirQuality(data[0].main.aqi)} />
+        )}
+      </div>
+      {data.map((item) => (
+        <table key={item.dt} className="table air-quality-list">
+          <tr>
+            <th>AQI</th>
+            <td>{item.main.aqi}</td>
+          </tr>
+          <tr>
+            <th>CO</th>
+            <td>
+              {item.components.co}
+              {' '}
+              μg/m3
+            </td>
+          </tr>
+          <tr>
+            <th>NO</th>
+            <td>
+              {item.components.no}
+              {' '}
+              μg/m3
+            </td>
+          </tr>
+          <tr>
+            <th>NO2</th>
+            <td>
+              {item.components.no2}
+              {' '}
+              μg/m3
+            </td>
+          </tr>
+          <tr>
+            <th>O3</th>
+            <td>
+              {item.components.o3}
+              {' '}
+              μg/m3
+            </td>
+          </tr>
+          <tr>
+            <th>SO2</th>
+            <td>
+              {item.components.so2}
+              {' '}
+              μg/m3
+            </td>
+          </tr>
+          <tr>
+            <th>PM2.5</th>
+            <td>
+              {item.components.pm2_5}
+              {' '}
+              μg/m3
+            </td>
+          </tr>
+          <tr>
+            <th>PM10</th>
+            <td>
+              {item.components.pm10}
+              {' '}
+              μg/m3
+            </td>
+          </tr>
+          <tr>
+            <th>NH3</th>
+            <td>
+              {item.components.nh3}
+              {' '}
+              μg/m3
+            </td>
+          </tr>
+        </table>
       ))}
+
     </div>
   );
 };
 
 DetailsPage.propTypes = {
   fetchCoordinates: PropTypes.func.isRequired,
-  data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({
+    dt: PropTypes.number.isRequired,
+  })).isRequired,
   loading: PropTypes.bool.isRequired,
   error: PropTypes.string,
 };
@@ -122,7 +173,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCoordinates: (lat, lon) => dispatch(fetchCoordinates(lat, lon)),
+  fetchCoordinates: (stateName) => dispatch(fetchCoordinates(stateName)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DetailsPage);
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,12 @@
-// src/components/Filter.js
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { FiSettings, FiChevronRight } from 'react-icons/fi'; // <-- Agrega los iconos que necesitas aquí
+import { FaBell } from 'react-icons/fa';
 import { fetchCoordinates, selectState } from '../actions';
 import statesImages from '../data/mexico.json';
+import airPurityIcon from '../assets/air-purity.PNG';
 import '../styles/filter.css';
 
 const Filter = ({ fetchCoordinates, selectState }) => {
@@ -14,15 +16,28 @@ const Filter = ({ fetchCoordinates, selectState }) => {
   };
 
   return (
-    <div className="states-grid">
-      {statesImages.map(({ id, estado, url }) => (
-        <Link to={`/details/${estado}`} key={id} onClick={() => handleStateChange(estado)}>
-          <div className="state-container">
-            <img src={url} alt={estado} className="state-image" />
-            <div className="state-title">{estado}</div>
-          </div>
-        </Link>
-      ))}
+    <div>
+      <div className="logo-container">
+        <img src={airPurityIcon} alt="Air Purity Logo" className="app-logo" />
+      </div>
+
+      <div className="icon-container">
+        <FaBell className="icon" />
+        <FiSettings className="icon" />
+      </div>
+      <div className="states-grid">
+        {statesImages.map(({ id, estado, url }) => (
+          <Link to={`/details/${estado}`} key={id} onClick={() => handleStateChange(estado)}>
+            <div className="state-container">
+              <img src={url} alt={estado} className="state-image" />
+              <div className="state-title">{estado}</div>
+              <div className="state-arrow">
+                <FiChevronRight color="white" size="20px" />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
